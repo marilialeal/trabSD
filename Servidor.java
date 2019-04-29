620a069249bbcdac4567ba35929386d323febc7a
Marilia Leal <45775369+marilialeal@users.noreply.github.com>
2019-04-29 16:10:00 -0300

    Update Servidor.java

--- Servidor.java
+++ Servidor.java
@@ -1,20 +1,23 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.Map;
+import java.util.Arrays;
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Servidor extends Thread {
 
 	// Parte que controla as conexões por meio de threads.
 	// Note que a instanciação está no main.
-	private static Vector <PrintStream> Vetor_Clientes;
+	private static Vector <PrintStream> Fila_F1;
+	private Mapa mapa;
 	private Socket conexao;
-	private String opcao;
-	private Queue Fila_F1 = new LinkedList(); 
-	private Queue Fila_F2 = new LinkedList(); 
-	private Queue Fila_F3 = new LinkedList(); 
+	private int opcao;
 
 	public static void main(String args[]) {
 		// instancia o vetor de clientes conectados
-		Vetor_Clientes = new Vector <PrintStream> ();
+		Fila_F1 = new Vector <PrintStream> ();
 		try {
 			// criando um socket que fica escutando a porta 2222.
 			ServerSocket s = new ServerSocket(5082);
@@ -40,50 +43,74 @@ public class Servidor extends Thread {
 	}
 	public Servidor(Socket s) {
 		conexao = s;
+		mapa = new Mapa();
 	}
 	// execução da thread
 	public void run() {
 		try {
 			// objetos que permitem controlar fluxo de comunicação
 			BufferedReader entrada = new BufferedReader(new	InputStreamReader(conexao.getInputStream()));
+			Scanner ler = new Scanner(System.in);
 			PrintStream saida = new PrintStream(conexao.getOutputStream());
 			// primeiramente, espera-se a opcao do servidor 
+			BigInteger chave;
+			byte[] valor;
 			while(true){
-				entrada = entrada.readLine();
+				try{
+				opcao = Integer.parseInt(entrada.readLine());
 				// agora, verifica se string recebida é valida, pois
 				// sem a conexão foi interrompida, a string é null.
-				// Se isso ocorrer, deve-se terminar a execução. 
-				if (entrada == null) {return;}
-				else 
-				{
-                 // --------------------------------------------------------------------------------------------//
-				// funcao para receber os comandos do cliente e adicionar a fila F1			
-                try 
-				{
-                    Fila_F1.add(comando);
-                } catch (IllegalStateException e) {
-                e.printStackTrace();
-                } 
-				// adiciona o comando a Fila_F1 
-				// --------------------------------------------------------------------------------------------//
-				
-			
-    			System.out.println(conexao+" Opcao selecionada = "+opcao);
-				saida.println("Opcao selecionada = "+opcao);
+				// Se isso ocorrer, deve-se terminar a execução.
+				System.out.println("opcao ="+opcao);
+				if (opcao == 5){
+					break;
+				}
+				else if (opcao == 1){
+					saida.println("Opcao selecionada = \n"+opcao);
+					chave= new BigInteger(entrada.readLine());
+					//System.out.println(chave);
+					//saida.println("Entre com o valor:");
+					valor= entrada.readLine().getBytes();	
+					if(mapa.create(chave, valor) == 0) saida.println("Inserido com sucesso");
+					else saida.println("Erro na insercao");
+				}
+				else if (opcao == 2){
+					saida.println("Opcao selecionada = \n"+opcao);
+					chave = new BigInteger(entrada.readLine());
+					valor= mapa.read(chave);
+					String msgDecode = new String(valor); // convertendo byte para string
+					saida.println("Valorprocurado: "+ msgDecode); // Exibir
+					
+				}
+				else if (opcao == 3){
+					saida.println("Opcao selecionada = \n"+opcao);
+					chave= new BigInteger(entrada.readLine());
+					saida.println("Entre com o valor:");
+					valor= entrada.readLine().getBytes();	
+					if(mapa.update(chave,valor) == 0) saida.println("Atualizacao feita com sucesso");
+					else saida.println("Erro");
+				}
+				else if (opcao == 4){
+					//saida.println("Opcao selecionada = "+opcao+"Entre com a chave:");
+					chave= new BigInteger(entrada.readLine());
+					if(mapa.delete(chave) == 0) saida.println("Exclusao feita com sucesso");
+					else saida.println("Erro");
+				}
 				// Uma vez que se tem um cliente conectado
 				// coloca-se fluxo de saída para esse cliente no vetor de
 				// clientes conectados.
-				Vetor_Clientes.add(saida);
-				// Vetor_Clientes é objeto compartilhado por várias threads!
+				Fila_F1.add(saida);
+				// Fila_F1 é objeto compartilhado por várias threads!
 				// De acordo com o manual da API, os métodos são
 				// sincronizados. Portanto, não há problemas de acessos
 				// simultâneos.
 				// Verificar se linha é null (conexão interrompida)
 				// Se não for nula, pode-se compará-la com métodos string
+				}catch(NumberFormatException e){}
 			}
 			// Uma vez que o cliente enviou linha em branco, retira-se
 			// fluxo de saída do vetor de clientes e fecha-se conexão.
-			Vetor_Clientes.remove(saida);
+			Fila_F1.remove(saida);
 			conexao.close();
 		}
 		catch (IOException e) {
@@ -93,76 +120,44 @@ public class Servidor extends Thread {
 	}
 }
 
+class Mapa{
+   
+	private Map<BigInteger, byte[]> mapa;
 
+	public Mapa(){
+	    	this.mapa = new HashMap<>();
+	}
+	 
+	public boolean existe(BigInteger o1) {
+	    	if (mapa.get(o1) == null) return false;
+			else return true;
+	}
 
+	public int create(BigInteger o1, byte[] o2){
+		if (!existe(o1)){
+			mapa.put(o1,o2);
+			return 0;
+		}else return -1;
+	}
+	
+  	public int update(BigInteger o1, byte[] o2){
+		if(existe(o1)){
+			mapa.remove(o1);
+			mapa.put(o1,o2);
+			return 0;		
+		}
+		else return 1;
+	}
 
-				
-				// --------------------------------------------------------------------------------------------//
-				// funcao para remover os comandos da Fila_F1 e adicionar nas Filas 2 e 3
-				String Comeco_Fila = null;
-                try {
-                Comeco_Fila = Fila_F1.remove();
-				} catch (NoSuchElementException e) 
-				{
-					e.printStackTrace();
-			    }
-				Fila_F2.add(Comeco_Fila);
-				Fila_F2.add(Comeco_Fila);
-				// --------------------------------------------------------------------------------------------//
-				
-				
-				// --------------------------------------------------------------------------------------------//
-				// funcao para remover os comandos da Fila_F2 e gravar no disco 
-				String Operacao = null;
-                try {
-                Operacao = Fila_F2.remove();
-				} catch (NoSuchElementException e) 
-				{
-					e.printStackTrace();
-			    } 
-				//implementar gravacao no disco usando a string Operacao
-				// --------------------------------------------------------------------------------------------//
-				
-				
-				// --------------------------------------------------------------------------------------------//
-				// funcao para remover os comandos da Fila_F3 e aplicar no banco
-				String Operacao = null;
-                try {
-                Operacao = Fila_F3.remove();
-				} catch (NoSuchElementException e) 
-				{
-					e.printStackTrace();
-			    }
-				String[] comando = Operacao.split(';') //Divide o comando recebido em duas partes separadas por ;
-				opcao = Operacao[0]  // primeira parte é a opcao CRUD				
-				//"1 - Create"
-				//"2 - Read"
-				//"3 - Update"
-				//"4 - Delete"
-				funcao = Operacao[1]  // segunda parte é a operacao 
-				if (opcao == "1")//"1 - Create"
-				{	
-			
-				}
-				else if (opcao == "2")//"2 - Read"
-				{
-					
-				}
-				else if (opcao == "3")//"3 - Update"
-				{
-					
-				}
-				else if (opcao == "4")//"4 - Delete"
-				{
-					
-				}
-				else if (opcao == "5")//"4 - SAIR"
-				{					
-					break;
-				}
-					
-					
-				}
-				// --------------------------------------------------------------------------------------------//
-				
-				
+	public int delete(BigInteger o1){
+		if(existe(o1)){
+			mapa.remove(o1);
+			return 0;		
+		}
+		else return 1;
+	}
+	
+	public byte[] read(BigInteger o1){
+		return mapa.get(o1);
+	}
+}
